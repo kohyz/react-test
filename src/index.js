c8bd34ead7dfec44382001d6bcff45547d3bdd04
Koh Yee Zuo <kohyeezuo@hotmail.com>
2022-03-14 00:17:24 +0800

    websocket demo

--- src/index.js
+++ src/index.js
@@ -1,5 +1,23 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// import socketio and create new server
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  socket.on("chat message", (msg) => {
+    // This will emit the event to all connected sockets
+    io.emit("chat message", msg);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
