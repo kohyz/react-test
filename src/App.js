5d042ea0acb23b396602789b8e797668fc807761
Koh Yee Zuo <kohyeezuo@hotmail.com>
2022-03-13 23:11:51 +0800

    basic react

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import Todo from "./components/Todo";
-
 function App() {
   return (
     <div>
-      <h1>My Todos</h1>
-      <Todo text="A" />
-      <Todo text="B" />
-      <Todo text="C" />
-      <Todo text="D" />
-      <Todo />
+      <h1>Hi</h1>
     </div>
   );
 }
